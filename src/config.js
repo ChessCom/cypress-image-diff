d19cac3875ef2feca467ada10d69ef0f2bf01e51
Greg Holguin <simulatedgreg@gmail.com>
2022-04-20 15:35:00 -0500

    add ability to customize paths

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
 import path from 'path'
 
-const parentDirFolderName = 'cypress-visual-screenshots'
+const parentDirFolderName = process.env.CYPRESS_IMAGE_DIFF_DIRECTORY as string ?? 'cypress-visual-screenshots';
 const parentDir = path.join(process.cwd(), parentDirFolderName)
 const baseline = path.join(process.cwd(), parentDirFolderName, 'baseline')
 const comparison = path.join(process.cwd(), parentDirFolderName, 'comparison')
 const diff = path.join(process.cwd(), parentDirFolderName, 'diff')
-const reportDir = path.join(process.cwd(), 'cypress-visual-report')
+const reportDir = path.join(
+  process.cwd(),
+  path.join(process.env.CYPRESS_IMAGE_DIFF_DIRECTORY, 'reports') ?? 'cypress-visual-report'
+);
 
 const paths = {
   image: {
